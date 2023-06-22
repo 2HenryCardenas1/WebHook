d81b18ccda4a1860fd895726478f1f98b7a86b59
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-06-22 10:11:12 -0500

    logic edited

--- webhook.js
+++ webhook.js
@@ -10,22 +10,16 @@ const URL = "https://datosprueba-7893.twil.io/datosPrueba.json";
 
 app.use(express.json()); // For parsing application/json
 
-axios
-  .get(URL)
-  .then((response) => {
-    if (response.status !== 200) {
-      throw new Error(`Request failed with status code ${response.status}`);
-    }
-    return response.data;
-  })
-  .then((data) => {
-    // Write the data to the file
-    app.get("/webhook", (req, res) => {
+app.post("/webhook", (req, res) => {
+  const URL = req.body.endPoint;
+  axios
+    .get(URL)
+    .then((response) => {
       const dataLog = {
-        Date: formatDate(data.TimestampMs),
-        TypeEvent: data.EventType,
-        ConversationSid: data.TaskAttributes.conversationSid,
-        TaskAttributes: data.TaskAttributes.data,
+        Date: formatDate(response.data.TimestampMs),
+        TypeEvent: response.data.EventType,
+        ConversationSid: response.data.TaskAttributes.conversationSid,
+        TaskAttributes: response.data.TaskAttributes.data,
       };
 
       const log = JSON.stringify(dataLog) + "\n";
@@ -33,12 +27,27 @@ axios
         if (err) throw err;
       });
 
-      res.send("OK, view the log file");
+      res.status(200).send("Ok");
+    })
+    .catch((error) => {
+      switch (error.response.status) {
+        case 400:
+          console.error("Error 400: Bad Request");
+          res.status(400).send("Error 400: Bad Request");
+          break;
+        case 401:
+          console.error("Error 401: Unauthorized");
+          res.status(401).send("Error 401: Unauthorized");
+          break;
+        case 500:
+          console.error("Error 500: Internal Server Error");
+          res.status(500).send("Error 500: Internal Server Error");
+          break;
+        default:
+          res.status(500).send("Error al obtener los datos del archivo JSON");
+      }
     });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+});
 
 app.get("/webhook.log", (req, res) => {
   res.send(fs.readFileSync(LOG_FILE, "utf8"));
